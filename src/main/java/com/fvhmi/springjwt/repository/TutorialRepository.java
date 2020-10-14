0422cef6c0b306c248de631818e166bd6e685182
fahmi amir <amirfahmi8@gmail.com>
2020-10-14 10:21:12 +0700

    pagination crud

--- src/main/java/com/fvhmi/springjwt/repository/TutorialRepository.java
+++ src/main/java/com/fvhmi/springjwt/repository/TutorialRepository.java
@@ -2,11 +2,13 @@ package com.fvhmi.springjwt.repository;
 
 import java.util.List;
 
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fvhmi.springjwt.models.Tutorial;
 
 public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
-    List<Tutorial> findByPublished(boolean published);
-    List<Tutorial> findByTitleContaining(String title);
+    Page<Tutorial> findByPublished(boolean published, org.springframework.data.domain.Pageable paging);
+    Page<Tutorial> findByTitleContaining(String title, org.springframework.data.domain.Pageable paging);
 }

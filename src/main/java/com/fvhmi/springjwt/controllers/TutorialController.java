5663d51e7071167720aa26e9eea015aa142a16aa
fahmi amir <amirfahmi8@gmail.com>
2020-10-15 08:20:35 +0700

    sort crud

--- src/main/java/com/fvhmi/springjwt/controllers/TutorialController.java
+++ src/main/java/com/fvhmi/springjwt/controllers/TutorialController.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -35,25 +37,52 @@ public class TutorialController {
     @Autowired
     TutorialRepository tutorialRepository;
 
+    private Sort.Direction getSortDirection(String direction) {
+        if (direction.equals("asc")) {
+          return Sort.Direction.ASC;
+        } else if (direction.equals("desc")) {
+          return Sort.Direction.DESC;
+        }
+
+        return Sort.Direction.ASC;
+    }
+
     @GetMapping("/tutorials")
-    public ResponseEntity<Map<String, Object>> getAllTutorials(
-            @RequestParam(required = false) String title,
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "3") int size
-        ) {
+    public ResponseEntity<Map<String, Object>> getAllTutorialsPage(
+        @RequestParam(required = false) String title,
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "3") int size,
+        @RequestParam(defaultValue = "id,desc") String[] sort) {
 
         try {
-            List<Tutorial> tutorials = new ArrayList<Tutorial>();
-            Pageable paging = PageRequest.of(page, size);
-            
-            Page<Tutorial> pageTuts;
-            if (title == null) {
-                pageTuts = tutorialRepository.findAll(paging);
+            List<Order> orders = new ArrayList<Order>();
+
+            if (sort[0].contains(",")) {
+                // will sort more than 2 fields
+                // sortOrder="field, direction"
+                for (String sortOrder : sort) {
+                String[] _sort = sortOrder.split(",");
+                orders.add(new Order(getSortDirection(_sort[1]), _sort[0]));
+                }
             } else {
-                pageTuts = tutorialRepository.findByTitleContaining(title, paging);
+                // sort=[field, direction]
+                orders.add(new Order(getSortDirection(sort[1]), sort[0]));
             }
-            
+
+            List<Tutorial> tutorials = new ArrayList<Tutorial>();
+            Pageable pagingSort = PageRequest.of(page, size, Sort.by(orders));
+
+            Page<Tutorial> pageTuts;
+            if (title == null)
+                pageTuts = tutorialRepository.findAll(pagingSort);
+            else
+                pageTuts = tutorialRepository.findByTitleContaining(title, pagingSort);
+
             tutorials = pageTuts.getContent();
+
+            if (tutorials.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
 
             Map<String, Object> response = new HashMap<>();
             response.put("tutorials", tutorials);
--- src/main/java/com/fvhmi/springjwt/repository/TutorialRepository.java
+++ src/main/java/com/fvhmi/springjwt/repository/TutorialRepository.java
@@ -2,13 +2,17 @@ package com.fvhmi.springjwt.repository;
 
 import java.util.List;
 
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fvhmi.springjwt.models.Tutorial;
 
 public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
-    Page<Tutorial> findByPublished(boolean published, org.springframework.data.domain.Pageable paging);
-    Page<Tutorial> findByTitleContaining(String title, org.springframework.data.domain.Pageable paging);
+    Page<Tutorial> findByPublished(boolean published, Pageable pageable);
+
+    Page<Tutorial> findByTitleContaining(String title, Pageable pageable);
+
+    List<Tutorial> findByTitleContaining(String title, Sort sort);
 }
